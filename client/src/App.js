4b3603346cfbcdcaf6796da7ed949dc10076a25c
Tricia Chow <suyeechow0212@gmail.com>
2023-09-12 21:52:32 +0800

    Add back button

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import Home from "./pages/Home";
 import Onboarding from "./pages/Onboarding";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -8,13 +7,13 @@ import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminAddNew from "./pages/AdminAddNew";
 import AllAnimals from "./pages/AllAnimals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Navigate to="/signup" />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/onboarding/:id" element={<Onboarding />} />
@@ -23,7 +22,7 @@ const App = () => {
 				<Route path="/matches/:id" element={<Matches />} />
 				<Route path="/animal-profile/:id" element={<AnimalProfile />} />
 
-				<Route path="/admin/login" element={<AdminLogin />} />
+				{/* <Route path="/admin/login" element={<AdminLogin />} /> */}
 				<Route path="/admin/dashboard" element={<AdminDashboard />} />
 				<Route path="/admin/add-new" element={<AdminAddNew />} />
 			</Routes>
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Logo from "../assets/fosterly.svg";
+import { useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const Nav = () => {
+	let navigate = useNavigate();
+	const goBack = () => {
+		navigate(-1);
+	};
+
 	return (
 		<nav className="shadow-sm bg-white flex flex-row justify-between w-full h-20 items-center px-[140px] mb-12">
+			<div
+				onClick={goBack}
+				className="cursor-pointer flex gap-x-2 text-sm justify-center items-center"
+			>
+				<ArrowLeft size={16} /> Back
+			</div>
 			<img src={Logo} alt="Logo" />
+
 			<a
 				href="/all"
 				className="text-sm font-semibold text-primary-600 underline"
